26dc6e267e2d765e486bc2fc7ecffb11f42b72f3
sajjadul-hub <sajjadulislamsp@gmail.com>
2023-01-13 19:40:10 +0600

    dokan-server

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-
+/////////////////////////////////////////////////////////////////////////////////////////
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.p1jrtk0.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
